514169e2a9a77215f8e1737694e40dd1ec73ab11
AlluSushma2806 <130675793+AlluSushma2806@users.noreply.github.com>
2026-01-18 07:54:09 +0530

    Fix module declaration for VM configuration

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,6 @@
-module "vm" {
+ module "vm" {
   source       = "./modules/vm"
-  vm_name      = local.vm_config[local.env].vm_name
   zone         = var.zone
   machine_type = local.vm_config[local.env].machine_type
 }
+
